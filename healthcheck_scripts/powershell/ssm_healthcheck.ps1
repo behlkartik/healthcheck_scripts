e62063968d0dc766b1a9ff06a01477f2f95ff483
Kartik Behl <kart1813@YYTR9369X5.local>
2023-01-12 16:31:41 +0530

    fixed powershell healthcheck script

--- healthcheck_scripts/powershell/ssm_healthcheck.ps1
+++ healthcheck_scripts/powershell/ssm_healthcheck.ps1
@@ -15,12 +15,13 @@ $are_services_expected_status = $true
 $services_not_expected_status = @()
 foreach($service in $services) 
  { 
-    $serviceName=$service.Name
-    $service.Refresh()
+    $service_info=$(Get-Service -Name "$service")
+    $serviceName=$service_info.Name
+    $service_info.Refresh()
     $serviceStatus = $service.Status
     if ( $serviceStatus -ne $EXPECTED_SERVICE_STATUS ){
         $are_services_expected_status = $false
-        $services_not_expected_status += @("$serviceName")
+        $services_not_expected_status += "$serviceName"
     }
  } 
 
